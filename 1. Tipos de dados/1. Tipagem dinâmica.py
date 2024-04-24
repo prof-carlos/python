f4ecbbcff747dcf205fe42b2d9b0b2583d841062
prof-carlos <128708765+prof-carlos@users.noreply.github.com>
2024-04-24 14:18:30 +0000

    Exercícios de iniciais de Python.

---
+++ 1. Tipos de dados/1. Tipagem dinâmica.py
@@ -0,0 +1,10 @@
+# Tipagem dinâmica.
+nome = "Marta"
+idade = 20
+peso = 50
+altura = 1.71
+
+print(nome)
+print(idade)
+print(peso)
+print(altura)
---
+++ 1. Tipos de dados/3. Solicitando dados.py
@@ -0,0 +1,16 @@
+# Solicitando dados para o usuário.
+
+nome: str
+idade: int
+peso: float
+altura: float 
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite o peso: "))
+altura = float(input("Digite usa altura: "))
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Peso: {peso}")
+print(f"Altura: {altura}")
